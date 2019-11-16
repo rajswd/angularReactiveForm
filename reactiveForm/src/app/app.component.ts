493f78583461d6480a7a3963f94cc31296b88bdd
rajswd <raj.swd1@gmail.com>
2019-11-17 00:25:13 +0530

    style changes

--- reactiveForm/src/app/app.component.ts
+++ reactiveForm/src/app/app.component.ts
@@ -10,11 +10,12 @@ export class AppComponent implements OnInit {
 
   dataModel:Array<any> = [];
   formModel:FormGroup;
-  _counter:number = 0;
+
 
   constructor(private fb:FormBuilder){
     this.formModel = this.fb.group({
       fName:['', Validators.required],
+      mName:[''],
       lName:['', Validators.required],
       id:['', Validators.required],
       idType:['', Validators.required],
@@ -26,15 +27,19 @@ export class AppComponent implements OnInit {
     return this.formModel.get('companyList') as FormArray;
   }
 
+  getName(item):string{
+    return item.fName +" " + item.mName+ " "+item.lName;
+  }
+
   addcompanyList(index:number) {
-    let a = "cName_"+this._counter,
-        b = "wLocation_"+this._counter;
 
     this.companyList.push(this.fb.group({
       cName:['',Validators.required],
-      wLocation:['',Validators.required]
+      wLocation:['',Validators.required],
+      tDuration:['']
     }));
-    this._counter++;
+    // this.dataModel = this.formModel.value;//get("companyList") as FormArray
+    
   }
 
   ngOnInit(){
@@ -42,8 +47,22 @@ export class AppComponent implements OnInit {
 
   }
 
+  reset(index:number){
+    let cList = this.formModel.get("companyList") as FormArray;
+    cList.at(index).reset();
+  }
+
+  delItem(index:number){
+    let cList = this.formModel.get("companyList") as FormArray;
+    cList.removeAt(index);
+  }
+
   addToDataList(){
-    console.log(this.formModel.value, this.formModel.valid);
+    if(this.formModel.valid){
+      this.dataModel.push(this.formModel.value);//, this.formModel.valid);
+      console.log(this.dataModel);
+      this.formModel.reset();
+    }
   }
 
 
